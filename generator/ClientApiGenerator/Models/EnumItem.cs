f7bb98a300d36b7fb547393208a5bb729b46e103
ted-spence-avalara <ted.spence@avalara.com>
2016-11-22 11:45:17 -0800

    Refactor client API generator to support more render targets

--- generator/ClientApiGenerator/Models/EnumItem.cs
+++ generator/ClientApiGenerator/Models/EnumItem.cs
@@ -1,4 +1,4 @@
-﻿namespace ClientApiGenerator
+﻿namespace ClientApiGenerator.Models
 {
     public class EnumItem
     {
---
+++ generator/ClientApiGenerator/Render/BaseRenderTarget.cs
@@ -0,0 +1,21 @@
+﻿using ClientApiGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApiGenerator.Render
+{
+    public class BaseRenderTarget
+    {
+        /// <summary>
+        /// Render this particular type of client library
+        /// </summary>
+        /// <param name="model"></param>
+        /// <param name="rootPath"></param>
+        public virtual void Render(ApiModel model, string rootPath)
+        {
+        }
+    }
+}
