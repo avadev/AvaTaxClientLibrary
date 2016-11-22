f0385e38bb5582dd079a07f0d624d7c1503f591d
ted-spence-avalara <ted.spence@avalara.com>
2016-11-22 12:45:21 -0800

    Working on the packaging script

--- scripts/package.csharp.nuget.cmd
+++ scripts/package.csharp.nuget.cmd
@@ -1,6 +1,9 @@
 @echo off
 pushd .
 cd ..\clients\dotnet 
-dotnet build -c release
+dotnet restore
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe Avalara.AvaTax.RestClient.net20.csproj /property:Configuration=Release
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe Avalara.AvaTax.RestClient.netstandard.csproj /property:Configuration=Release
 ..\..\..\..\nuget.exe pack Avalara.AvaTax.RestClient.nuspec
 popd
+pause
