018602053c558ead45f7e023e87e130ec3d308b0
ted-spence-avalara <ted.spence@avalara.com>
2017-03-17 15:53:05 -0700

    AvaTaxConnect program for simpliftesting

---
+++ AvaTaxConnect/AvaTax-Connect/AvaTax-Connect/Options.cs
@@ -0,0 +1,32 @@
+﻿using Avalara.AvaTax.RestClient;
+using CommandLine;
+using System;
+
+namespace AvaTax_Connect
+{
+    public class Options
+    {
+        [Option(shortName: 'u', Required = true, HelpText = "Username for AvaTax.")]
+        public string Username { get; set; }
+
+        [Option(shortName: 'p', Required = true, HelpText = "Password for AvaTax.")]
+        public string Password { get; set; }
+
+        [Option(shortName: 'c', Required = true, HelpText = "Number of calls to make to AvaTax.  If null, will continue until cancelled.")]
+        public int? Calls { get; set; }
+
+        [Option(shortName: 'e', Required = false, HelpText = "AvaTax environment to call.")]
+        public AvaTaxEnvironment Environment { get; set; }
+
+        [Option(shortName: 'l', Required = false, HelpText = "Number of lines to include in each tax transaction (Default 1).")]
+        public int Lines { get; set; }
+
+        /// <summary>
+        /// Returns true if the options are valid
+        /// </summary>
+        public bool IsValid()
+        {
+            return (!String.IsNullOrEmpty(Username) && !String.IsNullOrEmpty(Password));
+        }
+    }
+}
---
+++ AvaTaxConnect/AvaTax-Connect/AvaTax-Connect/Program.cs
@@ -0,0 +1,56 @@
+﻿using Avalara.AvaTax.RestClient;
+using CommandLine;
+using CommandLine.Text;
+using System;
+
+namespace AvaTax_Connect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parse options and show helptext if insufficient
+            Options o = new Options();
+            Parser.Default.ParseArguments(args, o);
+            if (!o.IsValid()) {
+                var help = HelpText.AutoBuild(o);
+                Console.WriteLine(help.ToString());
+                return;
+            }
+
+            // Set up AvaTax
+            var client = new AvaTaxClient("AvaTax-Connect", "1.0", Environment.MachineName, o.Environment)
+                .WithSecurity(o.Username, o.Password);
+
+            // Use transaction builder
+            var tb = new TransactionBuilder(client, "DEFAULT", DocumentType.SalesOrder, "ABC")
+                .WithAddress(TransactionAddressType.SingleLocation, "123 Main Street", null, null, "Irvine", "CA", "92615", "US");
+
+            // Add lines
+            for (int i = 0; i < o.Lines; i++) {
+                tb.WithLine(100.0m);
+            }
+            var ctm = tb.GetCreateTransactionModel();
+
+            // Connect to AvaTax and print debug information
+            int count = 0;
+            while (true) {
+                count++;
+                if (o.Calls.HasValue && count > o.Calls.Value) {
+                    Console.WriteLine("Done.");
+                    return;
+                } else {
+                    Console.Write($"Call #{count}: ");
+                }
+
+                // Make one tax transaction
+                DateTime start = DateTime.UtcNow;
+                var t = client.CreateTransaction(ctm);
+                TimeSpan ts = DateTime.UtcNow - start;
+
+                // Write some information
+                Console.WriteLine($"Completed in {ts.TotalMilliseconds}ms; tax is {t.totalTax}.");
+            }
+        }
+    }
+}
