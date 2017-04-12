56b4ebe7f3bbcb9cde1f99490fea783694a86375
ted-spence-avalara <ted.spence@avalara.com>
2017-04-12 13:47:36 -0700

    Refactored swagger generator to work using anonymous templates

---
+++ generator/ClientApiGenerator/Render/RenderFixupTask.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApiGenerator.Render
+{
+    public class RenderFixupTask
+    {
+        /// <summary>
+        /// The path of the file on which to execute a fixup
+        /// </summary>
+        public string filePath { get; set; }
+
+        /// <summary>
+        /// The file encoding of the file (e.g. UTF8 or ASCII)
+        /// </summary>
+        public string encoding { get; set; }
+
+        /// <summary>
+        /// The regular expression to execute
+        /// </summary>
+        public string regex { get; set; }
+
+        /// <summary>
+        /// The replacement string to apply when the regex is found
+        /// </summary>
+        public string replacement { get; set; }
+    }
+}
