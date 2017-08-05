223dd429db641c2db4a851c95d39ba7ce9bc701e
Ted Spence <ted.spence@avalara.com>
2017-08-05 13:08:44 -0700

    Experimenting with WPF demo program

---
+++ examples/dotnet/AvaTaxDesktop/AvaTaxDesktop/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalara.AvaTax.RestClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AvaTaxDesktop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Instance of the AvaTax client for this app
+        /// </summary>
+        public static AvaTaxClient Client { get; set; }
+    }
+}
---
+++ examples/dotnet/AvaTaxDesktop/AvaTaxDesktop/Pages/LoginPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AvaTaxDesktop.Pages
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public string Username
+        {
+            get
+            {
+                return txtUsername.Text;
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return txtPassword.Password;
+            }
+        }
+
+        public string Environment
+        {
+            get
+            {
+                var cbi = cbxEnvironment.SelectedItem as ComboBoxItem;
+                if (cbi == null || cbi.Tag == null) return null;
+                return cbi.Tag.ToString();
+            }
+        }
+
+        public LoginPage()
+        {
+            InitializeComponent();
+
+            // Load previously saved settings
+            txtUsername.Text = Properties.Settings.Default.Username;
+            txtPassword.Password = Properties.Settings.Default.Password;
+            foreach (var customEnvironment in Properties.Settings.Default.CustomEnvironments.Split(',')) {
+                cbxEnvironment.Items.Add(new ComboBoxItem()
+                {
+                    Content = customEnvironment,
+                    Tag = customEnvironment
+                });
+            }
+            foreach (ComboBoxItem item in cbxEnvironment.Items) {
+                if (item.Tag != null) {
+                    item.IsSelected = (item.Tag.ToString() == Properties.Settings.Default.Environment);
+                }
+            }
+        }
+
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            App.Client = new Avalara.AvaTax.RestClient.AvaTaxClient("AvaTaxDesktop", "1.0", "", new Uri(Environment))
+                .WithSecurity(Username, Password);
+            var pingResult = await App.Client.PingAsync();
+            if (pingResult.authenticated == true) {
+
+                // Save settings
+                if (chkRemember.IsChecked == true) {
+                    Properties.Settings.Default.Username = txtUsername.Text;
+                    Properties.Settings.Default.Password = txtPassword.Password;
+                } else {
+                    Properties.Settings.Default.Username = "";
+                    Properties.Settings.Default.Password = "";
+                }
+                Properties.Settings.Default.Environment = Environment;
+                Properties.Settings.Default.Save();
+
+                // Go to the invoice page
+                MainWindow.Instance.frmMain.Navigate(new InvoicePage());
+            }
+        }
+
+        private void cbxEnvironment_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var cbi = cbxEnvironment.SelectedItem as ComboBoxItem;
+            if (cbi != null) {
+                if (cbi.Tag == null) {
+                    // TODO - Pop up messagebox and ask for other url
+                }
+            }
+        }
+    }
+}
