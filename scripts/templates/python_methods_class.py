c3dae3ec65d7e446bc71d3672e9355880df72d7c
HAN BAO <hbao2016@hotmail.com>
2018-03-06 15:52:47 -0800

    formed python tempalte for method generations

--- scripts/templates/python_methods_class.py
+++ scripts/templates/python_methods_class.py
@@ -1,77 +1,59 @@
-
 import requests
 from _str_version import str_type
 
 
-/*
- * AvaTax Software Development Kit for JavaScript
- *
- * (c) 2004-2018 Avalara, Inc.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * @@author     Han Bao <han.bao@@avalara.com>
- * @@author     Phil Werner
- * @@author     Robert Bronson
- * @@author     Adrienne Karnoski
- * @@copyright  2004-2018 Avalara, Inc.
- * @@license    https://www.apache.org/licenses/LICENSE-2.0
- * @@version    @SwaggerModel.ApiVersion
- * @@link       https://github.com/avadev/AvaTax-REST-V2-Python-SDK
- */
-
 class Mixin:
+    """Mixin class contain methods attached to Client class."""
+@foreach(var m in SwaggerModel.Methods){
+    var paramlist = new System.Text.StringBuilder();
+    var paramcomments = new System.Collections.Generic.List<string>();
+    string querystringoptions = "";
+    string callwithquerystring = "";
+	string payload = "None";
+	paramlist.Append("self, ");
+    foreach (var p in m.Params) {
+        if (p.CleanParamName == "X-Avalara-Client") continue;
+        if (p.ParameterLocation == ParameterLocationType.UriPath || p.ParameterLocation == ParameterLocationType.RequestBody) {
+            paramlist.Append(p.CleanParamName);
+            paramlist.Append(", ");
+        }
+        paramcomments.Add("      :param " + p.CleanParamName + " [" + PythonTypeName(p.TypeName) + "] " + PhpTypeComment(SwaggerModel, p) + "\r\n");
+        if (p.ParameterLocation == ParameterLocationType.QueryString) {
+            querystringoptions = "include=None";
+            callwithquerystring = "include";
+        }
 
-	@foreach(var m in SwaggerModel.Methods) {
-	    var paramlist = new System.Text.StringBuilder();
-	    var queryparamlist = new System.Text.StringBuilder();
-	    var paramcomments = new System.Collections.Generic.List<string>();
-	    string payload = "null";
-	    foreach (var p in m.Params) {
-	        if (p.CleanParamName == "X-Avalara-Client") continue;
-	        paramlist.Append(p.CleanParamName);
-	        paramlist.Append(", ");
-	        paramcomments.Add("\r\n     * @param " + JavascriptTypeName(p.TypeName) + " " + p.CleanParamName + " " + PhpTypeComment(SwaggerModel, p));
-	        
-	        if (p.ParameterLocation == ParameterLocationType.QueryString) {
-	            queryparamlist.Append("        " + p.ParamName + ": " + p.CleanParamName + ",\r\n");
-	        }
-	        if (p.ParameterLocation == ParameterLocationType.RequestBody) {
-	            payload = p.CleanParamName;
-	        }
-	    }
-	    if (paramlist.Length > 0) paramlist.Length -= 2;
-	    if (queryparamlist.Length > 0) queryparamlist.Length -= 3;
-
-	<text>
-
-     # @PythonComment(m.Summary, 6)
-      #
-      # @PythonComment(m.Description, 6)
-@if (paramcomments.Count > 0) {
-    foreach (var pc in paramcomments) { 
-        Write(pc);
+		if (p.ParameterLocation == ParameterLocationType.RequestBody) {
+			payload = p.CleanParamName;
+		}
     }
-}
+    if (!String.IsNullOrEmpty(querystringoptions)) {
+        paramlist.Append(querystringoptions);
+        paramlist.Append(", ");
+    }
 
-      # @@return [@PythonTypeName(m.ResponseTypeName)]
-      def @{Write(SnakeCase(m.Name) + "(" + paramlist.ToString() + ")");}
-        path = "@m.URI.Replace("{", "#{")"
-@{
-Write("        " + m.HttpVerb + "(path");
+    if (paramlist.Length > 0) paramlist.Length -= 2;
 
-if (m.BodyParam != null) {
-    Write(", model");
-}
-if (!String.IsNullOrEmpty(callwithquerystring)) {
-    Write(", " + callwithquerystring);
-}
-Write(")");
-}
-</text>
-}
-
-
-
-
+<text>
+    r"""
+    @PythonComment(m.Summary, 6)
+	
+    @PythonComment(m.Description, 6)
+	
+    @foreach (var pc in paramcomments) { Write(pc);}
+      :return @PythonTypeName(m.ResponseTypeName)
+    """
+    def @{Write(SnakeCase(m.Name) + "(" + paramlist.ToString() + "):");}
+        return requests.@{Write(m.HttpVerb.ToLower());}(f"{self.base_url}@m.URI", 
+@{Write("								auth=self.auth, headers=self.client_header,");}
+@if(callwithquerystring.Length > 0 && payload != "None"){
+Write("								params=" + callwithquerystring + ", json=" + payload);
+}else if(callwithquerystring.Length > 0){
+Write("								params=" + callwithquerystring);
+} else if(payload != "None") {
+Write("								json=" + payload);
+} else{
+Write("								params=None");	
+})
+</text>}
+ 
