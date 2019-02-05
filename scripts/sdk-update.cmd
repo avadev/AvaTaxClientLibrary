15c567a2d3e861b1655c77044d0af378c1b2cc2e
Genevieve Conty <contygm@gmail.com>
2019-02-04 16:50:21 -0800

    figured out json

--- scripts/sdk-update.cmd
+++ scripts/sdk-update.cmd
@@ -1,5 +1,5 @@
-SETLOCAL VERSION=NEXT-v
-SET JSON=\''{"title": "RELEAse v##.##.##", "head": "contygm:'"$VERSION"'", "base": "master", "maintainer_can_modify": true, "body": "huh zah"}'\'
+SET VERSION=NEXT-v
+SET JSON="{\"title\": \"RELEAse v##.##.##\", \"head\": \"contygm:%VERSION%\", \"base\": \"master\", \"maintainer_can_modify\": true, \"body\": \"huh zah\"}"
 
 START /WAIT "" "%PROGRAMFILES%\Git\bin\sh.exe" -c "sh git-branch.sh %VERSION%"
 
@@ -9,8 +9,8 @@ START /WAIT ClientApiGenerator.exe -g C:\git\develop\AvaTaxClientLibrary\scripts
 cd ..\..\..\..\scripts
 START /WAIT "" "%PROGRAMFILES%\Git\bin\sh.exe" -c "sh git-commit.sh %VERSION%"
 
-curl -H "User-Agent: contygm" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data $JSON https://api.github.com/repos/contygm/AvaTax-REST-V2-DotNet-SDK/pulls
-curl -H "User-Agent: contygm" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data $JSON https://api.github.com/repos/contygm/AvaTax-REST-V2-JS-SDK/pulls
-curl -H "User-Agent: contygm" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data $JSON https://api.github.com/repos/contygm/AvaTax-REST-V2-PHP-SDK/pulls
-curl -H "User-Agent: contygm" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data $JSON https://api.github.com/repos/contygm/AvaTax-REST-V2-Python-SDK/pulls
-curl -H "User-Agent: contygm" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data $JSON https://api.github.com/repos/contygm/AvaTax-REST-V2-Ruby-SDK/pulls
+curl -H "User-Agent: contygm" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data %JSON% https://api.github.com/repos/contygm/AvaTax-REST-V2-DotNet-SDK/pulls
+curl -H "User-Agent: contygm" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data %JSON% https://api.github.com/repos/contygm/AvaTax-REST-V2-JS-SDK/pulls
+curl -H "User-Agent: contygm" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data %JSON% https://api.github.com/repos/contygm/AvaTax-REST-V2-PHP-SDK/pulls
+curl -H "User-Agent: contygm" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data %JSON% https://api.github.com/repos/contygm/AvaTax-REST-V2-Python-SDK/pulls
+curl -H "User-Agent: contygm" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data %JSON% https://api.github.com/repos/contygm/AvaTax-REST-V2-Ruby-SDK/pulls
