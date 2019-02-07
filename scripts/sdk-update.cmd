bf419e38e3cf04b21d5df9b959a90c5e04819bf2
Genevieve Conty <genevieve.conty@avalara.com>
2019-02-07 12:03:29 -0800

    updated sdk update script so that the version and user agent are passed in

--- scripts/sdk-update.cmd
+++ scripts/sdk-update.cmd
@@ -1,6 +1,7 @@
-SET VERSION=v19.1.1
-SET JSON="{\"title\": \"Update for %VERSION%\", \"head\": \"avaDev:%VERSION%\", \"base\": \"master\", \"maintainer_can_modify\": true, \"body\": \"Update for %VERSION%\"}"
+SET VERSION=%1
+SET USER_AGENT=%2
 
+SET JSON="{\"title\": \"Update for %VERSION%\", \"head\": \"avaDev:%VERSION%\", \"base\": \"master\", \"maintainer_can_modify\": true, \"body\": \"Update for %VERSION%\"}"
 
 START /WAIT "" "%PROGRAMFILES%\Git\bin\sh.exe" -c "sh git-branch.sh %VERSION%"
 
@@ -10,8 +11,8 @@ START /WAIT ClientApiGenerator.exe -g C:\git\develop\AvaTaxClientLibrary\scripts
 cd ..\..\..\..\scripts
 START /WAIT "" "%PROGRAMFILES%\Git\bin\sh.exe" -c "sh git-commit.sh %VERSION%"
 
-curl -H "User-Agent: contygm" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data %JSON% https://api.github.com/repos/avaDev/AvaTax-REST-V2-DotNet-SDK/pulls
-curl -H "User-Agent: contygm" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data %JSON% https://api.github.com/repos/avaDev/AvaTax-REST-V2-JS-SDK/pulls
-curl -H "User-Agent: contygm" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data %JSON% https://api.github.com/repos/avaDev/AvaTax-REST-V2-PHP-SDK/pulls
-curl -H "User-Agent: contygm" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data %JSON% https://api.github.com/repos/avaDev/AvaTax-REST-V2-Python-SDK/pulls
-curl -H "User-Agent: contygm" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data %JSON% https://api.github.com/repos/avaDev/AvaTax-REST-V2-Ruby-SDK/pulls
+curl -H "User-Agent: %USER_AGENT%" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data %JSON% https://api.github.com/repos/avaDev/AvaTax-REST-V2-DotNet-SDK/pulls
+curl -H "User-Agent: %USER_AGENT%" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data %JSON% https://api.github.com/repos/avaDev/AvaTax-REST-V2-JS-SDK/pulls
+curl -H "User-Agent: %USER_AGENT%" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data %JSON% https://api.github.com/repos/avaDev/AvaTax-REST-V2-PHP-SDK/pulls
+curl -H "User-Agent: %USER_AGENT%" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data %JSON% https://api.github.com/repos/avaDev/AvaTax-REST-V2-Python-SDK/pulls
+curl -H "User-Agent: %USER_AGENT%" -H "Content-Type: application/json" -H "Authorization: token %GIT_TOKEN%" -X POST --data %JSON% https://api.github.com/repos/avaDev/AvaTax-REST-V2-Ruby-SDK/pulls
